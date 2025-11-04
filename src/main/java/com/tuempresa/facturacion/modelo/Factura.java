abde0202f0af68243b5fb84e34f3eb4ef9644efc
jriver123 <jjrivera@uamv.edu.ni>
2025-11-03 22:51:58 -0800

    Cambios de la leccion 4

--- src/main/java/com/tuempresa/facturacion/modelo/Factura.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -11,6 +11,13 @@ import org.openxava.calculators.*;
 import lombok.*;
 
 @Entity @Getter @Setter
+@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
+        "anyo, numero, fecha;" + // Separados por coma significa en la misma línea
+                "cliente;" + // Punto y coma significa nueva línea
+                "detalles;" +
+                "observaciones"
+)
+
 public class Factura {
 
     @Id
@@ -35,6 +42,7 @@ public class Factura {
     LocalDate fecha;
 
     @ManyToOne(fetch=FetchType.LAZY, optional=false) // El cliente es obligatorio
+    @ReferenceView("Simple") // La vista llamada 'Simple' se usará para visualizar esta referencia
     Cliente cliente;
 
     @ElementCollection
